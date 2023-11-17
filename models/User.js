9171e6e174e1a1659ce795eb4bd291e68ae3dbf4
Lorenzo <lorenzo-a@live.fr>
2023-11-17 17:49:52 +0100

    User model ok

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 20
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 8,
+        maxlength: 100
+    },
+    roles: [{
+        type: String,
+        default: "Employee"
+    }],
+    active: {
+        type: Boolean,
+        default: true
+    }
+})
+
+module.exports = mongoose.model('User', userSchema);
