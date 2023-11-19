ebb121564581b175328a072591208baebf106917
Lorenzo <lorenzo-a@live.fr>
2023-11-19 19:40:03 +0100

    Add UsersList

---
+++ frontend/src/features/users/User.jsx
@@ -0,0 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons"
+import { useNavigate } from 'react-router-dom'
+
+import { useSelector } from 'react-redux'
+import { selectUserById } from './usersApiSlice'
+
+const User = () => {
+    return (
+        <div>
+            User
+        </div>
+    );
+};
+
+export default User;
